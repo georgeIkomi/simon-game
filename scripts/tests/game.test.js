ba9aaed2bb46d09493dc8f71cb95c6e15864d81f
Eyituoyo George Ikomi <ikomieyituoyogeorge@yahoo.com>
2023-05-25 12:08:52 +0000

    Extend game functionality

--- scripts/tests/game.test.js
+++ scripts/tests/game.test.js
@@ -1,4 +1,4 @@
-const { game } = require("../game");
+const { game, newGame, showScore } = require("../game");
 
 beforeAll(() => {
     let fs = require("fs");
@@ -26,3 +26,25 @@ describe("game object contains correct keys", () => {
         expect(game.choices).toEqual(["button1", "button2", "button3", "button4"]);
     });
 });
+describe("newGame works correctly", () => {
+    beforeAll(() => {
+        game.score = 42;
+        game.playerMoves = [6, 19, 10];
+        game.currentGame = ["x", "y", "z"];
+        document.getElementById("score").innerText = "42";
+        newGame();
+    });
+    test("should set game score to zero", () => {
+        expect(game.score).toEqual(0);
+    });
+    test("should clear playerMoves array to empty array", () => {
+        expect(game.playerMoves.length).toBe(0);
+    });
+    test("should clear currentGame array to empty array", () => {
+        expect(game.currentGame.length).toBe(0);
+    });
+    test("should display 0 for the element with id 'score'", () => {
+        expect(document.getElementById("score").innerText).toEqual(0);
+    });
+});
+
